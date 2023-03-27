3ab6956c4e34049aec500c286f01074ee26c3700
amarshall88 <awmarsha@ncsu.edu>
2023-03-26 22:58:34 -0400

    Ground Truth Update

--- CNN.py
+++ CNN.py
@@ -1,8 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import PIL
-import tensorflow as tf
-
-from tensorflow import keras
+import pandas as pd
 
 
+
+#df = pd.read_csv('GroundTruth\GroundTruth.csv', names = ['MEL', 'NV', 'BCC', 'AKIEC', 'BKL', 'DF', 'VASC'])
+
+df = pd.read_csv('GroundTruth\GroundTruth.csv')
+
+print (df.head())
+print (len(df))
+print (df.columns)
+#labels=['MEL', 'NV', 'BCC', 'AKIEC', 'BKL', 'DF', 'VASC']
